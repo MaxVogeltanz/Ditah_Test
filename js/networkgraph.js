b651b931a07a0d6256d84549ec840bba47e402ab
Maximilian Vogeltanz <55436809+MaxVogeltanz@users.noreply.github.com>
2024-07-02 09:51:21 +0200

    Add files via upload

--- js/networkgraph.js
+++ js/networkgraph.js
@@ -1,9 +1,11 @@
 
         const nodes = [
+            /*3 basic nodes*/
             { id: "Infra", name: "Infrastruktur", category: "Säule", color: "#004567" },
             { id: "Methoden", name: "Methoden & Tools", category: "Säule", year: 2020, color: "#004567" },
             { id: "Wissenstransfer", name: "Wissentransfer", category: "Säule", year: 2020, color: "#004567" },
             
+            /*organisation nodes*/
             { id: "öaw", name: "ÖAW", category: "Organisationen", color: "#660000" },
             { id: "TU Wien", name: "TU Wien", category: "Organisationen", color: "#660000" },
             { id: "Innsbruck", name: "Uni Innsbruck", category: "Organisationen", color: "#660000" },
@@ -15,42 +17,68 @@
             { id: "Uni Wien", name: "Uni Wien", category: "Organisationen", color: "#660000" },
             { id: "onb", name: "ÖNB", category: "Organisationen", color: "#660000" },
             
-            { id: "gams", name: "GAMS", category: "Organisationen", color: "#666600" },
-            { id: "arche", name: "ARCHE", category: "Organisationen", color: "#666600" },
-            { id: "phaidra", name: "PHAIDRA", category: "Organisationen", color: "#666600" },
+            /*infrastructure-nodes*/
+            { id: "https://gams.uni-graz.at", name: "GAMS", category: "Infrastruktur", color: "#666600" },
+            { id: "https://arche.acdh.oeaw.ac.at", name: "ARCHE", category: "Infrastruktur", color: "#666600" },
+            { id: "phaidra.html", name: "PHAIDRA", category: "Infrastruktur", category2: "Organisationen", color: "#666600" },
             
+            /*project nodes*/
             { id: "fercan.html", name: "FERCAN", category: "Projekte", year: 2020, color: "#006600" },
             { id: "derla.html", name: "DERLA", category: "Projekte", category2: "MethodenTools", year: 2020, color: "#006600" },
             { id: "ficker.html", name: "Ficker-Edition", category: "Projekte", year: 2020, color: "#006600" },
             { id: "hps.html", name: "HPS", category: "Projekte", year: 2024, color: "#006600" },
             { id: "gipse.html", name: "GIPSE", category: "Projekte", year: 2024, color: "#006600" },
             { id: "digi16.html", name: "DIGI 16", category: "Projekte", year: 2024, color: "#006600" },
-            { id: "m3r.html", name: "M3R", category: "Projekte", year: 2023, color: "#006600" },
-            { id: "patrimonium.html", name: "PATRIMONIUM", category: "Projekte", year: 2023, color: "#006600" },
+            { id: "m3r.html", name: "M3R", category: "Projekte", category2: "MethodenTools", year: 2022, color: "#006600" },
+            { id: "patrimonium.html", name: "PATRIMONIUM", category: "Projekte", category2: "MethodenTools", year: 2023, color: "#006600" },
             { id: "onbkulturerbe.html", name: "Kulturerbe", category: "Projekte", year: 2023, color: "#006600" },
             { id: "textvsvisual.html", name: "Text vs Visual", category: "Projekte", year: 2023, color: "#006600" },
             { id: "holzmeister.html", name: "Holzmeister", category: "Projekte", year: 2023, color: "#006600" },
-            { id: "fotodb.html", name: "Fotodatenbank", category: "Projekte", year: 2023, color: "#006600" },
+            { id: "fotodatenbank.html", name: "Fotodatenbank", category: "Projekte", year: 2023, color: "#006600" },
             { id: "rnab.html", name: "RNAB", category: "Projekte", year: 2023, color: "#006600" },
+            { id: "dhlehre.html", name: "DH Lehre", category: "Projekte", year: 2023, color: "#006600" },
+            { id: "habsburg.html", name: "Habsburg", category: "Projekte", year: 2021, color: "#006600" },
+            { id: "dhplus.html", name: "DH Plus", category: "Projekte", color: "#660000" },
 
         ];
 
         const links = [
             { source: "Infra", target: "Methoden" },
             { source: "Infra", target: "Wissenstransfer" },
+            
+            { source: "Infra", target: "habsburg.html" },
+            { source: "Infra", target: "phaidra.html" },
+            { source: "Infra", target: "onbkulturerbe.html" },
+            { source: "Infra", target: "rnab.html" },
+            { source: "Infra", target: "fotodatenbank.html" },
+            { source: "Infra", target: "holzmeister.html" },
+            { source: "Infra", target: "ficker.html" },
+            
             { source: "Methoden", target: "Wissenstransfer" },
+            
             { source: "Methoden", target: "ficker.html" },
             { source: "Methoden", target: "holzmeister.html" },
             { source: "Methoden", target: "gipse.html" },
             { source: "Methoden", target: "hps.html" },
+            { source: "Methoden", target: "m3r.html" },
+            { source: "Methoden", target: "patrimonium.html" },
+            { source: "Methoden", target: "fercan.html" },
+            { source: "Methoden", target: "derla.html" },
+            { source: "Methoden", target: "onbkulturerbe.html" },
+            { source: "Methoden", target: "rnab.html" },
+            { source: "Methoden", target: "fotodatenbank.html" },
             
-            { source: "Wissenstransfer", target: "plus" },
-            
-            { source: "Infra", target: "gams" },
-            { source: "Infra", target: "arche" },
-            { source: "Infra", target: "phaidra" },
-            { source: "gams", target: "unigraz" },
-            { source: "arche", target: "öaw" },
+            { source: "Wissenstransfer", target: "derla.html" },
+            { source: "Wissenstransfer", target: "dhplus.html" },
+            { source: "Wissenstransfer", target: "dhlehre.html" },
+            { source: "Wissenstransfer", target: "habsburg.html" },
+             { source: "Wissenstransfer", target: "onbkulturerbe.html" },
+             
+            { source: "Infra", target: "https://gams.uni-graz.at" },
+            { source: "Infra", target: "https://arche.acdh.oeaw.ac.at" },
+            { source: "Infra", target: "phaidra.html" },
+            { source: "https://gams.uni-graz.at", target: "unigraz" },
+            { source: "https://arche.acdh.oeaw.ac.at", target: "öaw" },
             
             { source: "unigraz", target: "derla.html" },
             { source: "unigraz", target: "hps.html" },
@@ -61,9 +89,11 @@
             { source: "unigraz", target: "textvsvisual.html" },
             { source: "Innsbruck", target: "ficker.html" },
             { source: "Innsbruck", target: "holzmeister.html" },
-            { source: "Innsbruck", target: "fotodb.html" },
+            { source: "Innsbruck", target: "fotodatenbank.html" },
             { source: "Innsbruck", target: "rnab.html" },
-            { source: "Uni Wien", target: "phaidra" },
+            { source: "Uni Wien", target: "phaidra.html" },
+            { source: "Uni Wien", target: "habsburg.html" },
+            { source: "Uni Wien", target: "dhlehre.html" },
             { source: "Cima", target: "Uni Wien" },
             { source: "Cima", target: "Uni Krems" },
             { source: "Cima", target: "TU Wien" },
@@ -71,12 +101,8 @@
             { source: "Cima", target: "m3r.html" },
             { source: "ABK", target: "m3r.html" },
             { source: "onb", target: "onbkulturerbe.html" },
+            { source: "plus", target: "dhplus.html" },
             
-            { source: "Methoden", target: "m3r.html" },
-            { source: "Methoden", target: "patrimonium.html" },
-            { source: "Methoden", target: "fercan.html" },
-            { source: "Methoden", target: "derla.html" },
-            { source: "Wissenstransfer", target: "derla.html" },
             { source: "TU Wien", target: "m3r.html" },
             { source: "Uni Krems", target: "m3r.html" },
             
@@ -243,6 +269,7 @@ applyZoomAndPan();
 // Filter nodes based on checkbox state and timeline value
 function updateGraph() {
     const showMethodenTools = d3.select("#filterMethodenTools").property("checked");
+    const showInfrastruktur = d3.select("#filterInfrastruktur").property("checked");
     const showOrganisationen = d3.select("#filterOrganisationen").property("checked");
     const showProjekte = d3.select("#filterProjekte").property("checked");
     const timelineValue = +d3.select("#timeline").property("value");
@@ -253,18 +280,26 @@ function updateGraph() {
         // Apply the OR logic for category 1, 2, 3
         // Every node can be inside up to 3 categories. depending on that checkbox filters will show or hide a node
         const categoryMatch = (node.category === "MethodenTools" && showMethodenTools) || 
+                              (node.category === "Infrastruktur" && showInfrastruktur) ||
                               (node.category === "Organisationen" && showOrganisationen) || 
                               (node.category === "Projekte" && showProjekte);
 
         const category2Match = (node.category2 === "MethodenTools" && showMethodenTools) || 
+                               (node.category2 === "Infrastruktur" && showMethodenTools) ||
                                (node.category2 === "Organisationen" && showOrganisationen) || 
                                (node.category2 === "Projekte" && showProjekte);
                                
         const category3Match = (node.category3 === "MethodenTools" && showMethodenTools) || 
+                               (node.category3 === "Infrastruktur" && showMethodenTools) ||
                                (node.category3 === "Organisationen" && showOrganisationen) || 
                                (node.category3 === "Projekte" && showProjekte);
+                               
+        const category4Match = (node.category4 === "MethodenTools" && showMethodenTools) || 
+                               (node.category4 === "Infrastruktur" && showMethodenTools) ||
+                               (node.category4 === "Organisationen" && showOrganisationen) || 
+                               (node.category4 === "Projekte" && showProjekte);
 
-        if (!(categoryMatch || category2Match || category3Match )) return false;
+        if (!(categoryMatch || category2Match || category3Match || category4Match )) return false;
 
         if (node.year > timelineValue) return false;
 
@@ -289,6 +324,7 @@ function updateGraph() {
 }
 
 d3.select("#filterMethodenTools").on("change", updateGraph);
+d3.select("#filterInfrastruktur").on("change", updateGraph);
 d3.select("#filterOrganisationen").on("change", updateGraph);
 d3.select("#filterProjekte").on("change", updateGraph);
 d3.select("#timeline").on("input", function() {
