cdc27ee8b7f856b3240d2e6b73e567b1f93d89c5
ElisabethRaunig <45683496+ElisabethRaunig@users.noreply.github.com>
2024-09-03 15:28:20 +0200

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -30,7 +30,7 @@ function scrolldown (src) {
 			window.setTimeout(function () {
 				window.location.href = src;
 				// 2. zahl ist wie weit gescollt wird; minus nach unten; plus nach oben;
-				window.scrollBy(0, -75);
+				window.scrollBy(0, -125);
 			},
 			100);
 		};
